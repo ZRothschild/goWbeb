ee20d298916b9e14e0414dd2cbac8ef9ccd24fd3
ZRothschild <873908960@qq.com>
2018-10-29 21:36:00 +0800

    1.使用处理程序

--- apiDoc/cookies/cookies.md
+++ apiDoc/cookies/cookies.md
@@ -1,3 +1,8 @@
+# cookies使用
+
+### 基础cookies操作
+
+```go
 package main
 
 import "github.com/kataras/iris"
@@ -48,3 +53,55 @@ func main() {
 	// DELETE: http://localhost:8080/cookies/my_name
 	app.Run(iris.Addr(":8080"))
 }
+```
+
+### 如何加密cookies
+
+```go
+package main
+//开发人员可以使用任何库添加自定义cookie编码器/解码器。
+//在这个例子中，我们使用gorilla的securecookie包：
+// $ go get github.com/gorilla/securecookie
+// $ go run main.go
+
+import (
+	"github.com/kataras/iris"
+	"github.com/gorilla/securecookie"
+)
+
+var (
+	// AES仅支持16,24或32字节的密钥大小。
+	//您需要准确提供该密钥字节大小，或者从您键入的内容中获取密钥。
+	hashKey  = []byte("the-big-and-secret-fash-key-here")
+	blockKey = []byte("lot-secret-of-characters-big-too")
+	sc       = securecookie.New(hashKey, blockKey)
+)
+
+func newApp() *iris.Application {
+	app := iris.New()
+	app.Get("/cookies/{name}/{value}", func(ctx iris.Context) {
+		name := ctx.Params().Get("name")
+		value := ctx.Params().Get("value")
+		ctx.SetCookieKV(name, value, iris.CookieEncode(sc.Encode)) // <--设置一个Cookie
+		ctx.Writef("cookie added: %s = %s", name, value)
+	})
+	app.Get("/cookies/{name}", func(ctx iris.Context) {
+		name := ctx.Params().Get("name")
+		value := ctx.GetCookie(name, iris.CookieDecode(sc.Decode)) // <--检索Cookie
+		ctx.WriteString(value)
+	})
+	app.Delete("/cookies/{name}", func(ctx iris.Context) {
+		name := ctx.Params().Get("name")
+		ctx.RemoveCookie(name) // <-- 删除Cookie
+		ctx.Writef("cookie %s removed", name)
+	})
+	return app
+}
+
+func main() {
+	app := newApp()
+	app.Run(iris.Addr(":8080"))
+}
+```
+
+[Go Web Iris中文网](https://www.studyiris.com/)
--- apiDoc/cookies/securecookie/main.go
+++ apiDoc/cookies/securecookie/main.go
@@ -1,19 +1,17 @@
 package main
-
-// developers can use any library to add a custom cookie encoder/decoder.
-// At this example we use the gorilla's securecookie package:
+//开发人员可以使用任何库添加自定义cookie编码器/解码器。
+//在这个例子中，我们使用gorilla的securecookie包：
 // $ go get github.com/gorilla/securecookie
 // $ go run main.go
 
 import (
 	"github.com/kataras/iris"
-
 	"github.com/gorilla/securecookie"
 )
 
 var (
-	// AES only supports key sizes of 16, 24 or 32 bytes.
-	// You either need to provide exactly that amount or you derive the key from what you type in.
+	// AES仅支持16,24或32字节的密钥大小。
+	//您需要准确提供该密钥字节大小，或者从您键入的内容中获取密钥。
 	hashKey  = []byte("the-big-and-secret-fash-key-here")
 	blockKey = []byte("lot-secret-of-characters-big-too")
 	sc       = securecookie.New(hashKey, blockKey)
@@ -21,35 +19,22 @@ var (
 
 func newApp() *iris.Application {
 	app := iris.New()
-
-	// Set A Cookie.
 	app.Get("/cookies/{name}/{value}", func(ctx iris.Context) {
 		name := ctx.Params().Get("name")
 		value := ctx.Params().Get("value")
-
-		ctx.SetCookieKV(name, value, iris.CookieEncode(sc.Encode)) // <--
-
+		ctx.SetCookieKV(name, value, iris.CookieEncode(sc.Encode)) // <--设置一个Cookie
 		ctx.Writef("cookie added: %s = %s", name, value)
 	})
-
-	// Retrieve A Cookie.
 	app.Get("/cookies/{name}", func(ctx iris.Context) {
 		name := ctx.Params().Get("name")
-
-		value := ctx.GetCookie(name, iris.CookieDecode(sc.Decode)) // <--
-
+		value := ctx.GetCookie(name, iris.CookieDecode(sc.Decode)) // <--检索Cookie
 		ctx.WriteString(value)
 	})
-
-	// Delete A Cookie.
 	app.Delete("/cookies/{name}", func(ctx iris.Context) {
 		name := ctx.Params().Get("name")
-
-		ctx.RemoveCookie(name) // <--
-
+		ctx.RemoveCookie(name) // <-- 删除Cookie
 		ctx.Writef("cookie %s removed", name)
 	})
-
 	return app
 }
 
